00dd6e4cd2bb2c3dd00c9dcc67adc509602ce3c6
Dan Thurman <dan.thurman@buddymedia.com>
2013-06-06 15:23:59 -0400

    almost there

--- test/config/config.js
+++ test/config/config.js
@@ -3,6 +3,6 @@ module.exports = {
 "rules" : [
             function(){/* 'Print op and let it live.' */ console.log(JSON.stringify(this)); return true;},
             //function(){[>Kill everything over 2 secs! <] return (this.secs_running < 2);}
-            function(){ return !(this.op === "query" && this.ns === "twisk.t1" && this.secs_running > 1);}
+            function(){ return !(this.op === "query" && this.ns === "publisher.posts" && this.secs_running > 1);}
        ]
 }
