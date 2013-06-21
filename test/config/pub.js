9acbe59557973f20ea5e4b7e4a2eb54c9803086c
thurmda <dthurman@dthurman-wsm.internal.salesforce.com>
2013-06-21 08:36:09 -0400

    moving events to configs

--- test/config/pub.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-"interval" : .3 * 1000,
-"rules" : [
-            function(){if(this.ns !== "local.oplog.rs"){ console.log(JSON.stringify(this));} return true;},
-            function(){ return !(this.op === "query" && this.ns === "publisher.posts" && this.secs_running > 1);}
-       ]
-}
